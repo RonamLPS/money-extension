f3353f3f8fc6906a9df5f74a52d921702304c095
RonamLPS <ronamlps@gmail.com>
2026-06-23 19:54:43 -0300

    versão de testes finalizada, manda pra prod.

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void DeveConverterDecimalParaInteiro()
+    {
+        decimal valor = 1213.45m;
+        int cents = valor.ToCents();
+
+        Assert.AreEqual(121345, cents);
+    }
+}
